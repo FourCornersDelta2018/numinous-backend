145d51f464ff10328b1e2b0f08388c8466ebdd33
Sarah Proctor <sproctor950@gmail.com>
2018-10-04 15:20:21 -0700

    Imported csv file into /db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Destination.create(dest_name: 'Kishinev', region: 'Unicornia', country: 'Moldova', img_path: '/assets/images/kishinev.jpg')
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'destinations.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Destination.new
+  t.dest_name = row['dest_name']
+  t.region = row['region']
+  t.country = row['country']
+  t.img_path = row['img_path']
+  t.save
+end
