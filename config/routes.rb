3abc93258e93f3f9a9541bc796d26cf133322107
Caitlin Johnson <caitlin.ch.johnson@gmail.com>
2018-10-11 15:32:50 -0700

    fixed the router issue lol

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :destinations, only: [:index]
-  resources :destination_users
+  resources :destinations, :destination_users
   devise_for :users, defaults: { format: :json }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
