8aa246db541cfe63e4724e645f7f8bd102c01142
Caitlin Johnson <caitlin.ch.johnson@gmail.com>
2018-10-11 14:36:51 -0700

    Added filtering functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :destinations, :destination_users
+  resources :destinations, only: [:index]
+  resources :destination_users
   devise_for :users, defaults: { format: :json }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
